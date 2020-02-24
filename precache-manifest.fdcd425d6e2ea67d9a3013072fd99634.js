17718a110cb49621c9348bf190411ae8a2b83d19
Sc222 <v.a.shaternikov@gmail.com>
2020-02-25 00:18:04 +0500

    Updates

---
+++ precache-manifest.fdcd425d6e2ea67d9a3013072fd99634.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3566b969b1efe9103e6cabd688de113",
+    "url": "/ettu-schedule/frontend/build/index.html"
+  },
+  {
+    "revision": "9ddbc9062e33d9e1d50d",
+    "url": "/ettu-schedule/frontend/build/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c82a34f153a558c2554",
+    "url": "/ettu-schedule/frontend/build/static/js/2.5980eb20.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ettu-schedule/frontend/build/static/js/2.5980eb20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddbc9062e33d9e1d50d",
+    "url": "/ettu-schedule/frontend/build/static/js/main.05d8affd.chunk.js"
+  },
+  {
+    "revision": "75319954bccbf36fe2b7",
+    "url": "/ettu-schedule/frontend/build/static/js/runtime-main.c0529f25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ettu-schedule/frontend/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
